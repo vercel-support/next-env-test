4214419b986aaf69c29995a130944165599bbaa8
Allen Hai <allen@vercel.com>
2020-12-09 19:50:53 -0600

    update readme

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ myVar: process.env.MY_VAR })
-}
+  res.statusCode = 200;
+  res.json({ myVar: process.env.MY_VAR, common: process.env.COMMON });
+};
